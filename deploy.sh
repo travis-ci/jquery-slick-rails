2bd6310865a30df5ddc28fde12834514989c0984
Kristofer Svardstal <kris@drtorte.net>
2018-05-11 12:56:37 -0400

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+body='{
+"request": {
+"branch":"master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $token" \
+   -d "$body" \
+   https://api.travis-ci.com/repo/travis-ci%2Frails-demo/requests
